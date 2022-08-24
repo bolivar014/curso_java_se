4a58e3bac268be454a8068a286f691280f284caa
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-08-24 13:16:15 -0500

    creación de bucle for

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,14 +1,11 @@
-public class WhileLoop {
+public class ForLoop {
     static boolean isTurnOnLight = false;
 
     public static void main(String[] args) {
-
         turnOnOffLight();
 
-        int i = 1;
-        while (isTurnOnLight && i<=10){
+        for (int i = 1; i <= 10; i++) {
             printSOS();
-            i++;
         }
     }
 
