25ee0a6f08cd214a6d92fded68460f60e0ed6020
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-08-24 09:56:24 -0500

    Convención de nombres de variables

---
+++ src/NamingJava.java
@@ -0,0 +1,19 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int cellphone = 3333333;
+        int cellPhone = 4444444;
+
+        System.out.println(cellphone);
+        System.out.println(cellPhone);
+
+        String $countryName = "Spain";
+        String _backgroundColor = "Green";
+        String currency$ = "MXN";
+        String background_Color = "Blue";
+
+        System.out.println($countryName);
+        System.out.println(_backgroundColor);
+        System.out.println(currency$);
+        System.out.println(background_Color);
+    }
+}
