0e2b52470e929dc4638268cb7002a9927a5e1c28
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-08-24 15:16:49 -0500

    inicializando arreglos

---
+++ src/Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // Inicialización de arreglos
+        // Arreglos de una dimensión
+        String[] androidVersions = new String[17];
+        String[] days = new String[7];
+
+        // Arreglos de dos dimensiones
+        String[][] cities = new String[4][2];
+
+        // Arreglos de tres dimensiones
+        int [][][] numbers = new int [2][2][2]; // filas - columnas - dimensiones
+    }
+}
