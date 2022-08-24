9306ef6bfd952c149e847bbaa249a4cb8a8a8cb2
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-08-24 09:34:53 -0500

    declaración de variables en java

---
+++ src/variables.java
@@ -0,0 +1,16 @@
+public class variables {
+    public static void main(String[] args) {
+        // Declaración de variables
+        int speed = 0;
+        int salary = 4;
+        String employee  = "Pepito Perez";
+
+        // Asignamos valor a variable
+        speed = 10;
+
+        // Imprimimos variable
+        System.out.println(speed);
+        System.out.println(salary);
+        System.out.println(employee);
+    }
+}
