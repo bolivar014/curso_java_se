3f83fb5c3231e423900ac4563ce5f8ebd56d90c5
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-08-24 10:24:25 -0500

    tipos de datos java

---
+++ src/DataType.java
@@ -0,0 +1,14 @@
+public class DataType {
+    public static void main(String[] args) {
+        int n = 1234567890;
+        long nL = 12345678901L;
+
+        double nD = 123.456;
+        float nF = 123.456F;
+
+        int salary = 1000;
+        // pensión 3%
+        double pension = salary*0.03;
+        System.out.println(pension);
+    }
+}
