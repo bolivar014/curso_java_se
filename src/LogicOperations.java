42c0808db1c1873fa865f6cc0536a5baedac38ae
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-08-24 12:08:45 -0500

    operadores logicos y expresiones booleanas

---
+++ src/LogicOperations.java
@@ -0,0 +1,18 @@
+public class LogicOperations {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+
+        // Operadores de asignación
+        System.out.println("A es igual a B? -> " + (a == b));
+        System.out.println("A es diferente a B? -> " + (a != b));
+
+        // Operadores relacionales
+        System.out.println("A es mayor a B? -> " + (a > b));
+        System.out.println("A es menor a B? -> " + (a < b));
+        System.out.println("A es mayor o igual a B? -> " + (a >= b));
+        System.out.println("A es menor o igual a B? -> " + (a <= b));
+
+        
+    }
+}
